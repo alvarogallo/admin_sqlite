22c6e285f0e248ee3c0df03c6dfe930c67c8e218
alvaro gallo <alvarogallo@hotmail.com>
2024-11-21 11:43:27 -0500

    Mas logs en la conexion 2 conexiones 1

--- src/server.js
+++ src/server.js
@@ -5,23 +5,23 @@ require('dotenv').config();
 
 async function testMySQLConnection() {
   try {
-    // Log connection parameters
-    // console.log('Attempting connection with:', {
-    //   host: 'containers-us-west-17.railway.app',
-    //   host: process.env.MYSQLUSER,
-    //   user: process.env.MYSQLUSER,
-    //   database: process.env.MYSQL_DATABASE,
-    //   port: process.env.MYSQLPORT
-    // });
-
-        // Log connection parameters
+    //Log connection parameters
     console.log('Attempting connection with:', {
-      mi_variable: process.env.MIVARIABLE,
-      database: process.env.MYSQL_DATABASE,
-      port: process.env.MYSQLPORT,
+      host: 'containers-us-west-17.railway.app',
+      host: process.env.MYSQLUSER,
       user: process.env.MYSQLUSER,
-      
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQLPORT
     });
+
+    //     // Log connection parameters
+    // console.log('Attempting connection with:', {
+    //   mi_variable: process.env.MIVARIABLE,
+    //   database: process.env.MYSQL_DATABASE,
+    //   port: process.env.MYSQLPORT,
+    //   user: process.env.MYSQLUSER,
+      
+    // });
     
 
     const connection = await mysql.createConnection({
